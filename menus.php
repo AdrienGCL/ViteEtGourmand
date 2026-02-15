58042236007e31d5e36189a7ba61d1a0e99b6a54
AdrienGCL <adriengicquel.3d@gmail.com>
2026-02-15 21:31:00 +0100

    Template menu_short.php WIP

--- menus.php
+++ menus.php
@@ -52,75 +52,9 @@
             </section>
             
             <section>
-                <div class="menuShort bg-extra-dark m-t-40">
-                    <div class="row justify-content-center p-t-40">
-                        <p class="headline-d primary-text col-auto m-b-10">Nom du menu</p>
-                    </div>
-                    <div class="row justify-content-center">
-                        <p class="text-d white-text col-auto">Description du menu, des différents plats.</p>
-                    </div>
-                    <div class="row justify-content-center p-t-40">
-                        <p class="text-d white-text col-auto m-0">Quantité minimum</p>
-                        <p class="headline-d secondary-text col-auto m-0">Prix/pers</p>
-                    </div>
-                    <div class="row justify-content-center p-t-40">
-                        <div class="btn-d bg-primary p-0 d-flex justify-content-center">
-                            <a class="text-d dark-text text-decoration-underline m-0 p-0" href="">Voir le menu</a>
-                        </div>
-                    </div>
-                    <div class="d-flex justify-content-center">
-                        <svg class="col-10 m-t-40 m-b-0 p-0" height="2" xmlns="http://www.w3.org/2000/svg">
-                            <line class="separator" x1="0" y1="0" x2="100%" y2="0"/>
-                            Sorry, your browser does not support inline SVG.
-                        </svg>
-                    </div>
-                </div>
-                <div class="menuShort bg-dark">
-                    <div class="row justify-content-center p-t-40">
-                        <p class="headline-d primary-text col-auto m-b-10">Nom du menu</p>
-                    </div>
-                    <div class="row justify-content-center">
-                        <p class="text-d white-text col-auto">Description du menu, des différents plats.</p>
-                    </div>
-                    <div class="row justify-content-center p-t-40">
-                        <p class="text-d white-text col-auto m-0">Quantité minimum</p>
-                        <p class="headline-d secondary-text col-auto m-0">Prix/pers</p>
-                    </div>
-                    <div class="row justify-content-center p-t-40">
-                        <div class="btn-d bg-primary p-0 d-flex justify-content-center">
-                            <a class="text-d dark-text text-decoration-underline m-0 p-0" href="">Voir le menu</a>
-                        </div>
-                    </div>
-                    <div class="d-flex justify-content-center">
-                        <svg class="col-10 m-t-40 m-b-0 p-0" height="2" xmlns="http://www.w3.org/2000/svg">
-                            <line class="separator" x1="0" y1="0" x2="100%" y2="0"/>
-                            Sorry, your browser does not support inline SVG.
-                        </svg>
-                    </div>
-                </div>
-                <div class="menuShort bg-extra-dark">
-                    <div class="row justify-content-center p-t-40">
-                        <p class="headline-d primary-text col-auto m-b-10">Nom du menu</p>
-                    </div>
-                    <div class="row justify-content-center">
-                        <p class="text-d white-text col-auto">Description du menu, des différents plats.</p>
-                    </div>
-                    <div class="row justify-content-center p-t-40">
-                        <p class="text-d white-text col-auto m-0">Quantité minimum</p>
-                        <p class="headline-d secondary-text col-auto m-0">Prix/pers</p>
-                    </div>
-                    <div class="row justify-content-center p-t-40">
-                        <div class="btn-d bg-primary p-0 d-flex justify-content-center">
-                            <a class="text-d dark-text text-decoration-underline m-0 p-0" href="">Voir le menu</a>
-                        </div>
-                    </div>
-                    <div class="d-flex justify-content-center">
-                        <svg class="col-10 m-t-40 m-b-0 p-0" height="2" xmlns="http://www.w3.org/2000/svg">
-                            <line class="separator" x1="0" y1="0" x2="100%" y2="0"/>
-                            Sorry, your browser does not support inline SVG.
-                        </svg>
-                    </div>
-                </div>
+                <?php include __DIR__. "/templates/menu_short.php"; ?>
+                <?php include __DIR__. "/templates/menu_short.php"; ?>
+                <?php include __DIR__. "/templates/menu_short.php"; ?>
             </section>
         </main>
 
---
+++ templates/menu_short.php
@@ -0,0 +1,23 @@
+<div class="menuShort m-t-40">
+    <div class="row justify-content-center p-t-40">
+        <p class="headline-d primary-text col-auto m-b-10">Nom du menu</p>
+    </div>
+    <div class="row justify-content-center">
+        <p class="text-d white-text col-auto">Description du menu, des différents plats.</p>
+    </div>
+    <div class="row justify-content-center p-t-40">
+        <p class="text-d white-text col-auto m-0">Quantité minimum</p>
+        <p class="headline-d secondary-text col-auto m-0">Prix/pers</p>
+    </div>
+    <div class="row justify-content-center p-t-40">
+        <div class="btn-d bg-primary p-0 d-flex justify-content-center">
+            <a class="text-d dark-text text-decoration-underline m-0 p-0" href="">Voir le menu</a>
+        </div>
+    </div>
+    <div class="d-flex justify-content-center">
+        <svg class="col-10 m-t-40 m-b-0 p-0" height="2" xmlns="http://www.w3.org/2000/svg">
+            <line class="separator" x1="0" y1="0" x2="100%" y2="0"/>
+            Sorry, your browser does not support inline SVG.
+        </svg>
+    </div>
+</div>
