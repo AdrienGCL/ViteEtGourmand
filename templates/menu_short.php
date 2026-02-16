ffa38714c8b40c3af5580cb241a20db9387c2dcd
AdrienGCL <adriengicquel.3d@gmail.com>
2026-02-16 20:47:21 +0100

    Création de la page avec le détail d'un menu.

--- templates/menu_short.php
+++ templates/menu_short.php
@@ -11,7 +11,7 @@
     </div>
     <div class="row justify-content-center padding-t-l">
         <div class="btn bg-primary p-0 d-flex justify-content-center">
-            <a class="text dark-text text-decoration-underline m-0 p-0" href="">Voir le menu</a>
+            <a class="text dark-text text-decoration-underline m-0 p-0" href="./detail_menu.php">Voir le menu</a>
         </div>
     </div>
     <div class="d-flex justify-content-center">
